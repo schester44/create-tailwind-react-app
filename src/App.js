d0921f29b98b051f263c1bfb80f1e0eb1f954b99
Steven Chester <steven.chester@industryweapon.com>
2019-11-25 16:13:02 -0500

    CRA + tailwindcss

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const App = () => <button className="px-4 hover:shadow-lg shadow py-2 m-24 text-white font-bold rounded bg-indigo-500">Hello World</button>
+
+export default App
